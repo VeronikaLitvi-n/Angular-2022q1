60039853252c1bf8d6b6a076ee0251e23fd748d5
weronika-fed <nikafed3@gmail.com>
2022-03-23 16:11:09 +0300

    feat: add basic structure

---
+++ youtube-client/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FiltersComponent } from './filters/filters.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchInputComponent } from './header/search-input/search-input.component';
+import { AuthenticationComponent } from './header/authentication/authentication.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FiltersComponent,
+    SearchItemComponent,
+    SearchResultsComponent,
+    SearchInputComponent,
+    AuthenticationComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
