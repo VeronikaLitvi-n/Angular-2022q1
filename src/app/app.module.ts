47c3752dd17b45154289e9becaf715c15ef0eff5
weronika-fed <nikafed3@gmail.com>
2022-03-24 22:36:42 +0300

    fix: fix import models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FiltersComponent } from './filters/filters.component';
+import { FiltersComponent } from './search/filters/filters.component';
 import { SearchItemComponent } from './search/search-item/search-item.component';
 import { SearchResultsComponent } from './search/search-results/search-results.component';
 import { SearchInputComponent } from './header/search-input/search-input.component';
 import { AuthenticationComponent } from './header/authentication/authentication.component';
-import { ModelsComponent } from './search/models/models.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +16,6 @@ import { ModelsComponent } from './search/models/models.component';
     SearchResultsComponent,
     SearchInputComponent,
     AuthenticationComponent,
-    ModelsComponent,
   ],
   imports: [
     BrowserModule,
